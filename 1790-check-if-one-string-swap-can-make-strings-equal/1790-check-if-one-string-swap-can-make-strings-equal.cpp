b581f08476fab76c55e797f910a4ecea88dcd5cb
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2022-12-03 12:28:58 +0530

    Time: 4 ms (45.03%), Space: 6.5 MB (5.09%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
+        //edge cases
         if(s1==s2) return true;
         if(s1.size()!=s2.size()) return false;
         
+        //checking if has same letters
         unordered_map<char,int> mp;
         for(char c:s1){
             mp[c]++;
@@ -15,6 +17,7 @@ public:
             if(i.second != 0) return false;
         }
         
+        //checking if only 2 char are different
         int count=0;
         for(int i=0; i<s1.size(); i++){
             if(s1[i]!=s2[i]) count++; 
