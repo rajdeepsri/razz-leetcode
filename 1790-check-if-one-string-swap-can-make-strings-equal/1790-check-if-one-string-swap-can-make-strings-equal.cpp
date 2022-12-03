71741f923f90c44548aaf4b350e63867fc07c9fb
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2022-12-03 12:34:07 +0530

    Time: 2 ms (65.03%), Space: 6.3 MB (22.36%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -5,24 +5,18 @@ public:
         if(s1==s2) return true;
         if(s1.size()!=s2.size()) return false;
         
-        //checking if has same letters
-        unordered_map<char,int> mp;
-        for(char c:s1){
-            mp[c]++;
-        }
-        for(char c:s2){
-            mp[c]--;
-        }
-        for(auto i:mp){
-            if(i.second != 0) return false;
-        }
-        
         //checking if only 2 char are different
         int count=0;
+        int a=0,b=0;
         for(int i=0; i<s1.size(); i++){
-            if(s1[i]!=s2[i]) count++; 
+            if(s1[i]!=s2[i]){
+                count++;
+                if(count==1) a=i;
+                if(count==2) b=i;
+                if(count>2) return false;
+            }
         }
-        if(count==2) return true;
-        return false;
+        swap(s1[a],s1[b]);
+        return s1==s2;
     }
 };
