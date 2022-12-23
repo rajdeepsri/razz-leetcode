297b5d98236fdf103ab189fd2eec6f7f2c30dd66
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2022-12-23 16:16:44 +0530

    Added solution - LeetHub

--- Reverse First K elements of Queue - GFG/reverse-first-k-elements-of-queue.cpp
+++ Reverse First K elements of Queue - GFG/reverse-first-k-elements-of-queue.cpp
@@ -32,7 +32,6 @@ int main() {
 
 // Function to reverse first k elements of a queue.
 queue<int> modifyQueue(queue<int> q, int k) {
-    //step 1 : pop first k element from the q and put into stack
     stack<int> st;
     int count=0;
     while(count<k){
@@ -40,19 +39,17 @@ queue<int> modifyQueue(queue<int> q, int k) {
         q.pop();
         count++;
     }
-    
-    //step 2 : fetch elements from stack and push into q
     while(!st.empty()){
         q.push(st.top());
         st.pop();
     }
     
-    //step 3 : fetch first (n-k) elements from q and pushback
-    int t = q.size() - k;
-    while(t--){
-        int val = q.front();
+    count=0;
+    while(count < q.size()-k){
+        int num=q.front();
         q.pop();
-        q.push(val);
+        q.push(num);
+        count++;
     }
     return q;
 }
