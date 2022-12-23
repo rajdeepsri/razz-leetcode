2e029c578860f2708810f4f5f0715b66ffe0427a
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2022-12-23 19:34:00 +0530

    Added solution - LeetHub

---
+++ Palindrome - GFG/palindrome.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    int num=n;
+		    int rev=0;
+		    while(num!=0){
+		        int dig = num%10;
+		        rev=rev*10+dig;
+		        num/=10;
+		    }
+		    if(rev==n) return "Yes";
+		    return "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
