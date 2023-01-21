af9eb2ab2a2a0b17edda4c94b868f5a8328d8305
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2023-01-21 18:20:01 +0530

    Time: 4 ms (34.77%), Space: 6.3 MB (48.32%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -3,9 +3,7 @@ public:
     int balancedStringSplit(string s) {
         int ans=0;
         int temp=0;
-        if(s[0]=='L') temp++;
-        else temp--;
-        for(int i=1; i<s.size(); i++){
+        for(int i=0; i<s.size(); i++){
             if(s[i]=='L') temp++;
             else temp--;
             if(temp==0) ans++;
