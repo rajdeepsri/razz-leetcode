5265f0792543535b58a559a5066ca7b6d83289f6
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2023-02-05 12:45:00 +0530

    Added solution - LeetHub

---
+++ Intersection Point in Y Shaped Linked Lists - GFG/intersection-point-in-y-shaped-linked-lists.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    if (size == 0) return NULL;
+
+    int val;
+    cin >> val;
+
+    Node *head = new Node(val);
+    Node *tail = head;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        cin >> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+
+// } Driver Code Ends
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution
+{
+public:
+    int intersectPoint(Node* head1, Node* head2){
+        // Your Code Here
+        Node* curr=head1;
+        while(curr->next!=NULL){
+            curr=curr->next;
+        }
+        curr->next=head2;
+        Node* fast=head1;
+        Node* slow=head1;
+        while(fast->next!=NULL && fast!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow) break;
+        }
+        
+        slow=head1;
+        while(fast!=slow){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        return slow->data;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+
+/* Driver program to test above function*/
+int main()
+{
+    int T, n1, n2, n3;
+
+    cin >> T;
+    while (T--)
+    {
+        cin >> n1 >> n2 >> n3;
+
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+
+        Node* temp = head1;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL) temp->next = common;
+
+        temp = head2;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL) temp->next = common;
+        
+        Solution ob;
+        cout << ob.intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
