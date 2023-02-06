5b03a1c9162d956c48c15cbcc89d8abf7b2db740
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2023-02-06 14:28:22 +0530

    Time: 11 ms (91.81%), Space: 8.7 MB (44.34%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -6,31 +6,25 @@ public:
         vector<int> phash(26, 0);
         int window = p.size();
         int len = s.size();
-        if(len < window)
-        {
-            return ans;
-        }
+        
+        if(len < window)  return ans;
         int left = 0,right = 0;
+        
         while(right < window)
         {
-            phash[p[right] - 'a'] += 1;
-            hash[s[right] - 'a'] += 1;
+            phash[p[right] - 'a'] ++;
+            hash[s[right] - 'a'] ++;
             right++;
         }
-        right -=1;
+        
+        right--;
         while(right < len)
         {
-            if(phash == hash)
-            {
-                ans.push_back(left);
-            }
-            right+=1;
-            if(right != len)
-            {
-                hash[s[right] - 'a'] += 1;
-            }
-            hash[s[left] - 'a'] -=1 ;
-            left += 1;
+            if(phash == hash) ans.push_back(left);
+            right++;
+            if(right != len) hash[s[right] - 'a'] ++;
+            hash[s[left] - 'a'] -- ;
+            left ++;
         }
         return ans;
     }
