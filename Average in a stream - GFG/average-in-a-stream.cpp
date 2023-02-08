a807c096111d9f968d6e04fb4552c27b9b9ca989
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2023-02-08 14:10:21 +0530

    Added solution - LeetHub

---
+++ Average in a stream - GFG/average-in-a-stream.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:	
+	vector<float> streamAvg(int arr[], int n) {
+	    // code here
+	    vector <float> v;
+	    float avg;
+	    float sum = 0;
+	    for (int i = 0 ; i < n ; i++){
+	        sum += arr[i];
+	        avg = sum/(i+1);
+	        v.push_back(avg);
+	    }
+	    return v;
+	}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.streamAvg(arr, n);
+        cout << fixed<< setprecision(2);
+        for (auto x : ans) {
+            cout <<x<<" ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
