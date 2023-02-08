0e999d0fc357010628cff491cea7dd8842f5fabe
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2023-02-08 14:02:58 +0530

    Added solution - LeetHub

---
+++ Print the left element - GFG/print-the-left-element.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int leftElement(int a[], int n) {
+        sort(a, a + n);
+        
+        int res;
+        int index = n / 2;
+        if(n%2) return a[index];
+        return a[index-1];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        cout << ob.leftElement(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
